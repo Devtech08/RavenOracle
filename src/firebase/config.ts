0e84f8140c7544d084e3fe39af9bb6d0e30be3d8
Prince <dadaprince011@gmail.com>
2026-02-07 13:12:13 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1124454172-c41ea",
+  "appId": "1:579496960065:web:fa9e32ccf3d05fb9f866c0",
+  "apiKey": "AIzaSyDSS8fKNfn71ILYDZOW_MCFjsdtX9wkaZc",
+  "authDomain": "studio-1124454172-c41ea.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "579496960065"
+};
